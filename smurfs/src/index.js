06a67643b9a3df8076aa476b85ce8dce63356036
JChartier001 <JChartier001@gmail.com>
2019-11-08 10:22:58 -0600

    imported dependencies and set up index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import {reducer} from "./reducers/reducers"
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, 
+document.getElementById("root"));
---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,8 @@
+export const initialState = {
+
+}
+
+export function reducers() {
+
+}
+
