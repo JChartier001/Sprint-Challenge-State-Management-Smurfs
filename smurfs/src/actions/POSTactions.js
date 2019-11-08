add43d50fc954ede2240a206728b50c827eb4877
JChartier001 <JChartier001@gmail.com>
2019-11-08 12:25:39 -0600

    finished postactions

--- smurfs/src/actions/POSTactions.js
+++ smurfs/src/actions/POSTactions.js
@@ -1,3 +1,32 @@
 import axios from "axios";
+import {FETCH_SMURFS_START, fetchSmurfs} from "./APIactions"
+import { NewSmurf } from "../components/NewSmurf";
+
 
 export const POST_NEW_SMURF = "POST_NEW_SMURF"
+export const POST_SMURF_SUCCESS = "POST_SMURF_SUCCESS"
+export const POST_SMURF_FAILURE = "POST_SMURF_FAILURE"
+
+
+
+
+export function postNewSmurf({ name, age, height}) {
+    return dispatch =>{
+        dispatch({type: FETCH_SMURFS_START});
+        axios
+        .post("http://localhost:3333/smurfs", {
+            name: NewSmurf.name,
+            age: NewSmurf.age,
+            heigh: NewSmurf.height
+        } )
+        .then(response => {
+            setTimeout(() => {
+                dispatch({type: POST_SMURF_SUCCESS, payload: response.data})
+            })          
+        
+        .catch(error => {
+            dispatch({type: POST_SMURF_FAILURE, payload: error})
+        });
+    })
+}
+}
--- smurfs/src/components/NewSmurf.js
+++ smurfs/src/components/NewSmurf.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux";
+import { postNewSmurf } from "../actions/POSTactions";
 
 
-const NewSmurf = () =>{
-    return (
-        <div>
-            <form>
-                <h2>Add a new Smurf Member</h2>
-                <label htmlFor="newSmurfName">Smurf Name: </label>
-                <input type="text" name="newSmurfName" placeholder="Enter Smurf Name"/>
-                <label htmlFor="newSmurfAge">Smurf Age: </label>
-                <input type="text" name="newSmurfAge" placeholder="Enter Smurf Age"/>
-                <label htmlFor="newSmurfHeight">Smurf Height: </label>
-                <input type="text" name="newSmurfHeight" placeholder="Enter Smurf Height"/>
-            </form>
-        </div>
-    )
+export const NewSmurf=() =>{
+    const [newSmurf, setNewSmurf] = useState({
+        name: "",
+        age: null,
+        height: ""
+    })
+    console.log("TCL: NewSmurf -> newSmurf", newSmurf)
+
+
+const handleInputChange = e =>{
+    setNewSmurf({[e.target.name]: e.target.value})
+    
 }
+
+const handleSubmit = e =>{
+    e.preventDefault();
+    postNewSmurf();
+}
+return (
+    
+    <div>
+        <form onClick={() => handleSubmit}>
+            <h2>Add a new Smurf Member</h2>
+            <label htmlFor="newSmurfName" >Smurf Name: </label>
+            <input type="text" name="newSmurfName" placeholder="Enter Smurf Name" onChange={handleInputChange}/>
+            <label htmlFor="newSmurfAge">Smurf Age: </label>
+            <input type="text" name="newSmurfAge" placeholder="Enter Smurf Age" onChange={handleInputChange}/>
+            <label htmlFor="newSmurfHeight">Smurf Height: </label>
+            <input type="text" name="newSmurfHeight" placeholder="Enter Smurf Height" onChange={handleInputChange}/>
+            <button>Submit</button>
+        </form>
+    </div>
+)
+}
+
+
+   
 
 function mapStateToProps(state) {
     return {
